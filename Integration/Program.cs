8f1af8794ae5165c42964e161fd98bf1513c6af3
fatih <fatih.ankakusu@gmail.com>
2024-05-27 23:02:20 +0300

    Add project files.

---
+++ Integration/Program.cs
@@ -0,0 +1,47 @@
+﻿using Integration.Service;
+
+namespace Integration;
+
+public abstract class Program
+{
+    public static void Main(string[] args)
+    {
+        //Server 1
+        var service = new ItemIntegrationService();
+        
+        ThreadPool.QueueUserWorkItem(_ => service.SaveDiffirentItems("a"));
+        ThreadPool.QueueUserWorkItem(_ => service.SaveDiffirentItems("b"));
+        ThreadPool.QueueUserWorkItem(_ => service.SaveDiffirentItems("c"));
+
+        Thread.Sleep(500);
+
+        ThreadPool.QueueUserWorkItem(_ => service.SaveDiffirentItems("a"));
+        ThreadPool.QueueUserWorkItem(_ => service.SaveDiffirentItems("b"));
+        ThreadPool.QueueUserWorkItem(_ => service.SaveDiffirentItems("c"));
+
+        Thread.Sleep(5000);
+        Console.WriteLine("Everything recorded Server 1:");
+
+        //Server 2
+        var service2 = new ItemIntegrationService();
+
+        ThreadPool.QueueUserWorkItem(_ => service2.SaveDiffirentItems("a"));
+        ThreadPool.QueueUserWorkItem(_ => service2.SaveDiffirentItems("b"));
+        ThreadPool.QueueUserWorkItem(_ => service2.SaveDiffirentItems("c"));
+
+        Thread.Sleep(500);
+
+        ThreadPool.QueueUserWorkItem(_ => service2.SaveDiffirentItems("d"));
+        ThreadPool.QueueUserWorkItem(_ => service2.SaveDiffirentItems("b"));
+        ThreadPool.QueueUserWorkItem(_ => service2.SaveDiffirentItems("e"));
+
+        Thread.Sleep(5000);
+
+        Console.WriteLine("Everything recorded Server 2:");
+
+        service.GetAllItems().ForEach(Console.WriteLine);
+        service2.GetAllItems().ForEach(Console.WriteLine);
+
+        Console.ReadLine();
+    }
+}
---
+++ Integration/Service/ItemIntegrationService.cs
@@ -0,0 +1,55 @@
+﻿using Integration.Common;
+using Integration.Backend;
+using System.Collections.Concurrent;
+
+namespace Integration.Service;
+
+public sealed class ItemIntegrationService
+{
+    //This is a dependency that is normally fulfilled externally.
+    private ItemOperationBackend ItemIntegrationBackend { get; set; } = new();
+
+    // This is called externally and can be called multithreaded, in parallel.
+    // More than one item with the same content should not be saved. However,
+    // calling this with different contents at the same time is OK, and should
+    // be allowed for performance reasons.
+    private static readonly ConcurrentDictionary<string, object> InProgressSaves = new();
+
+    public Result SaveItem(string itemContent)
+    {
+        // Check the backend to see if the content is already saved.
+        if (ItemIntegrationBackend.FindItemsWithContent(itemContent).Count != 0)
+        {
+            return new Result(false, $"Duplicate item received with content {itemContent}.");
+        }
+
+        var item = ItemIntegrationBackend.SaveItem(itemContent);
+
+        return new Result(true, $"Item with content {itemContent} saved with id {item.Id}");
+    }
+
+
+    public void SaveDiffirentItems(string itemContent)
+    {
+        // Lock object for the specific content to ensure thread safety
+        var lockObject = InProgressSaves.GetOrAdd(itemContent, new object());
+
+        lock (lockObject)
+        {
+            try
+            { 
+                SaveItem(itemContent);
+            }
+            finally
+            {
+            // Clean up the lock object if no other threads are waiting on it
+                InProgressSaves.TryRemove(itemContent, out _);
+            }
+        }
+    }
+
+    public List<Item> GetAllItems()
+    {
+        return ItemIntegrationBackend.GetAllItems();
+    }
+}
